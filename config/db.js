d11242bba30593f8dea9e103ff3d7cd1643e5661
YogitaModi <yogitamodi00@gmail.com>
2023-06-12 15:22:40 +0530

    created the server and connected to mongo db

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to mongodb database successfully ${conn.connection.host}`
+    );
+  } catch (error) {
+    console.log(`internal server error`);
+  }
+};
+module.exports = connectDb;
