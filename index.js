4288501bca56c3503930db6d46fb856b4aa507d1
YogitaModi <yogitamodi00@gmail.com>
2023-07-04 20:43:58 +0530

    index js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const auth = require("./routes/auth");
 const category = require("./routes/category");
 const product = require("./routes/product");
 const morgan = require("morgan");
-
+const path = require("path");
 dotenv.config();
 
 // connecting to mongodb
@@ -19,14 +19,14 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontened/build")));
 
 // createing routes
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/category", category);
 app.use("/api/v1/product", product);
-
-app.get("/", (req, res) => {
-  res.send("welcome to my app");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontened/build/index.html"));
 });
 
 app.listen(PORT, () => {
