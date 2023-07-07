e9bacbd39e47bc9a5e365dd6c087e732b3b89ad4
YogitaModi <yogitamodi00@gmail.com>
2023-07-07 16:54:24 +0530

    fixed path in index js file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const auth = require("./routes/auth");
 const category = require("./routes/category");
 const product = require("./routes/product");
 const morgan = require("morgan");
-const path = require("path");
+
 dotenv.config();
 
 // connecting to mongodb
@@ -19,15 +19,11 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontened/build")));
 
 // createing routes
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/category", category);
 app.use("/api/v1/product", product);
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../frontened/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log("Server is listening on port ", PORT);
