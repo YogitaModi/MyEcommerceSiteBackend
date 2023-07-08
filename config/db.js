3d60ceff469878a57ded66641fe2cfe4edc75c75
YogitaModi <yogitamodi00@gmail.com>
2023-07-08 17:13:13 +0530

    fixed connection problem

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
